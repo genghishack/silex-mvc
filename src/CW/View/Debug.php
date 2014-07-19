a6c3ce5763054fae926b0a7f59bbdf71054c0160
Chris Wade <chriswade@Chris-Wade-nio-MacBook-Pro-Retina.local>
2014-07-19 17:32:23 -0600

    removed vendor, .project, composer.lock from git and added to .gitignore

--- src/CW/View/Debug.php
+++ src/CW/View/Debug.php
@@ -2,14 +2,14 @@
 
 namespace CW\View;
 
-include_once(__DIR__.'/../../../vendor/techpatterns/php_browser_detection.php');
+include_once(__DIR__.'/../../php_browser_detection.php');
 
 class Debug extends \CW\View\Base {
 	
 	function getTemplateData() {
 		$foo =  array(
 			 'DebugVars'   => $this->app['debugger']->getDebugVars()
-			,'CompInfo'    => get_include_contents(__DIR__.'/../../../vendor/techpatterns/your_computer_info.php')
+			,'CompInfo'    => get_include_contents(__DIR__.'/../../your_computer_info.php')
 			,'Performance' => $this->app['debugger']->calculatePerformance()
 		);
 		// print_r ($foo);
